b1c6e5f1f4f041477f056ab34e1b9991c158b209
daniel-baumg <daniel88baumg@gmail.com>
2021-04-26 19:12:43 -0400

    Add files via upload

---
+++ blocks.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream fin("blocks.in");
+    ofstream fout("blocks.out");
+
+    int n;
+    fin >> n;
+    int result[26] = {};
+    for (int i = 0; i < n; i++) {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int blocks1[26] = {};
+        int blocks2[26] = {};
+        string word1, word2;
+        fin >> word1 >> word2;
+        for (int j = 0; j < word1.length(); j++)
+            blocks1[alphabet.find(word1[j])] += 1;
+        for (int j = 0; j < word2.length(); j++)
+            blocks2[alphabet.find(word2[j])] += 1;
+        for (int j = 0; j < 26; j++)
+            result[j] += max(blocks1[j], blocks2[j]);
+    }
+
+    for (int i = 0; i < 26; i++)
+        fout << result[i] << endl;
+}
