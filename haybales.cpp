15b90dc5135f195cebb62de322e9ce9f6924c480
Daniel Baumgartner <daniel88baumg@gmail.com>
2021-04-27 15:15:24 -0400

    Add files via upload

---
+++ haybales.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream fin("haybales.in");
+    ofstream fout("haybales.out");
+
+    int n, q;
+    fin >> n >> q;
+    vector<int> haybales(n);
+    for (int i = 0; i < n; i++)
+        fin >> haybales[i];
+    sort(haybales.begin(), haybales.end());
+    for (int i = 0; i < q; i++) {
+        int a, b;
+        fin >> a >> b;
+        fout << upper_bound(haybales.begin(), haybales.end(), b) - lower_bound(haybales.begin(), haybales.end(), a) << endl;
+    }
+}
