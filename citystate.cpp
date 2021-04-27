fad27443df376d6e7ce2a8c67787b4788f7e9173
Daniel Baumgartner <daniel88baumg@gmail.com>
2021-04-27 15:08:26 -0400

    Add files via upload

---
+++ citystate.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream fin("citystate.in");
+    ofstream fout("citystate.out");
+
+    int n;
+    fin >> n;
+    map<pair<string, string>, int> m;
+    for (int i = 0; i < n; i++) {
+        string city, state;
+        fin >> city >> state;
+        m[make_pair(city.substr(0, 2), state)] += 1;
+    }
+
+    int ans = 0;
+    for (pair<pair<string, string>, int> p : m) {
+        if (p.first.first == p.first.second)
+            continue;
+        ans += p.second * m[make_pair(p.first.second, p.first.first)];
+    }
+    fout << ans / 2 << endl;
+}
