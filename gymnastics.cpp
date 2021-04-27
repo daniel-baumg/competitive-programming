742c853617983f2375e19f76d383b189ca67caf8
Daniel Baumgartner <daniel88baumg@gmail.com>
2021-04-27 14:09:56 -0400

    Add files via upload

---
+++ gymnastics.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream fin("gymnastics.in");
+    ofstream fout("gymnastics.out");
+
+    int k, n;
+    fin >> k >> n;
+    int rankings[10][20];
+    for (int i = 0; i < k; i++)
+        for (int j = 0; j < n; j++)
+            fin >> rankings[i][j];
+    int ans = 0;
+    for (int a = 0; a < n; a++) {
+        for (int b = 0; b < n; b++) {
+            if (a == b)
+                continue;
+            bool consistent = true;
+            for (int i = 0; i < k; i++) {
+                int idxa = 0;
+                int idxb = 0;
+                for (int j = 0; j < n; j++) {
+                    if (rankings[i][j] == a + 1)
+                        idxa = j;
+                    if (rankings[i][j] == b + 1)
+                        idxb = j;
+                }
+                consistent &= idxa < idxb;
+            }
+            if (consistent)
+                ans += 1;
+        }
+    }
+    fout << ans << endl;
+}
