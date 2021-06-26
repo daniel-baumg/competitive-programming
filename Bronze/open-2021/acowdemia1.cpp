8ddb0e39be5968403e735fd0d3d082bb16b16ef3
Daniel Baumgartner <daniel88baumg@gmail.com>
2021-06-26 16:02:25 -0400

    Update acowdemia1.cpp

--- Bronze/open-2021/acowdemia1.cpp
+++ Bronze/open-2021/acowdemia1.cpp
@@ -1,34 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int c[100000];
-
-bool valid(int h) {
-    int num = 0;
-    for (int i = 0; i < n; i++)
-        if (c[i] >= h)
-            num += 1;
-    return num >= h;
-}
-
 int main() {
-    int l;
+    int n, l;
     cin >> n >> l;
+    int c[100000];
     for (int i = 0; i < n; i++)
         cin >> c[i];
     sort(c, c + n);
 
-    int h;
-    int lo = 0;
-    int hi = n;
-    while (lo <= hi) {
-        int mid = (lo + hi) / 2;
-        if (valid(mid)) {
-            h = mid;
-            lo = mid + 1;
-        } else {
-            hi = mid - 1;
+    int h = 0;
+    for (int i = 0; i < n; i++) {
+        if (c[i] >= n - i) {
+            h = n - i;
+            break;
         }
     }
 
