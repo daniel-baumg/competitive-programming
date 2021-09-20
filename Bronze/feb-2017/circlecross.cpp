43c148de0a000b4848cc571880f90a57d4c2475b
Daniel Baumgartner <daniel88baumg@gmail.com>
2021-09-20 19:38:29 -0400

    Update circlecross.cpp

--- Bronze/feb-2017/circlecross.cpp
+++ Bronze/feb-2017/circlecross.cpp
@@ -13,15 +13,11 @@ int main() {
         for (int j = i + 1; j < 26; j++) {
             vector<int> a, b;
             for (int k = 0; k < 52; k++) {
-                if (s[k] == alphabet[i])
-                    a.push_back(k);
-                if (s[k] == alphabet[j])
-                    b.push_back(k);
+                if (s[k] == alphabet[i]) a.push_back(k);
+                if (s[k] == alphabet[j]) b.push_back(k);
             }
-            if (a[0] < b[0] && b[0] < a[1] && a[1] < b[1])
-                ans += 1;
-            if (b[0] < a[0] && a[0] < b[1] && b[1] < a[1])
-                ans += 1;
+            if (a[0] < b[0] && b[0] < a[1] && a[1] < b[1]) ans += 1;
+            if (b[0] < a[0] && a[0] < b[1] && b[1] < a[1]) ans += 1;
         }
     }
     fout << ans << endl;
