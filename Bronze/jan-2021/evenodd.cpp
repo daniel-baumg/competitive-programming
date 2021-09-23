42b214bd52a59a134f56343be48261c681be9771
Daniel Baumgartner <daniel88baumg@gmail.com>
2021-09-23 14:15:33 -0400

    Update evenodd.cpp

--- Bronze/jan-2021/evenodd.cpp
+++ Bronze/jan-2021/evenodd.cpp
@@ -4,16 +4,14 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int o = 0;
     int e = 0;
     for (int i = 0; i < n; i++) {
         int id;
         cin >> id;
         if (id % 2 == 0)
             e += 1;
-        else
-            o += 1;
     }
+    int o = n - e;
 
     int ans = 0;
     while (true) {
