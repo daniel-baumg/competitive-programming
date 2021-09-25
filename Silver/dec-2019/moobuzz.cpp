e09e92e726377af80cbfaf20f61e13b746961a26
Daniel Baumgartner <daniel88baumg@gmail.com>
2021-09-24 23:28:34 -0400

    Update moobuzz.cpp

--- Silver/dec-2019/moobuzz.cpp
+++ Silver/dec-2019/moobuzz.cpp
@@ -8,6 +8,6 @@ int main() {
     int n;
     fin >> n;
     n -= 1;
-    int vals[] = { 1, 2, 4, 7, 8, 11, 13, 14 };
+    int vals[] = {1, 2, 4, 7, 8, 11, 13, 14};
     fout << (vals[n % 8] + n / 8 * 15) << endl;
 }
