17fb1c421760f3c6de1abe30c9b138d26e3d8d66
Daniel Baumgartner <daniel88baumg@gmail.com>
2021-09-29 15:16:03 -0400

    Update breedflip.cpp

--- Bronze/feb-2020/breedflip.cpp
+++ Bronze/feb-2020/breedflip.cpp
@@ -8,13 +8,10 @@ int main() {
     int n;
     string a, b;
     fin >> n >> a >> b;
-    bool equals[1000];
-    for (int i = 0; i < n; i++)
-        equals[i] = a[i] == b[i];
 
     int ans = 0;
     for (int i = 1; i < n; i++)
-        if (equals[i] && !equals[i - 1])
+        if (a[i] == b[i] && a[i - 1] != b[i - 1])
             ans += 1;
     fout << ans << endl;
 }
