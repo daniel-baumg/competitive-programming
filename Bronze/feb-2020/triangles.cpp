c0379e4982003b8dc85a6c06007456dbf6c71fe7
Daniel Baumgartner <daniel88baumg@gmail.com>
2021-09-29 14:01:19 -0400

    Update triangles.cpp

--- Bronze/feb-2020/triangles.cpp
+++ Bronze/feb-2020/triangles.cpp
@@ -15,18 +15,12 @@ int main() {
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             for (int k = j + 1; k < n; k++) {
-                if (x[i] == x[j] && y[i] == y[k])
-                    ans = max(ans, abs((x[i] - x[k]) * (y[i] - y[j])));
-                if (x[i] == x[j] && y[j] == y[k])
-                    ans = max(ans, abs((x[i] - x[k]) * (y[i] - y[j])));
-                if (x[i] == x[k] && y[i] == y[j])
-                    ans = max(ans, abs((x[i] - x[j]) * (y[i] - y[k])));
-                if (x[i] == x[k] && y[j] == y[k])
-                    ans = max(ans, abs((x[i] - x[j]) * (y[i] - y[j])));
-                if (x[j] == x[k] && y[i] == y[j])
-                    ans = max(ans, abs((x[i] - x[j]) * (y[i] - y[k])));
-                if (x[j] == x[k] && y[i] == y[k])
-                    ans = max(ans, abs((x[i] - x[j]) * (y[i] - y[j])));
+                if (x[i] == x[j] && y[i] == y[k]) ans = max(ans, abs((x[i] - x[k]) * (y[i] - y[j])));
+                if (x[i] == x[j] && y[j] == y[k]) ans = max(ans, abs((x[i] - x[k]) * (y[i] - y[j])));
+                if (x[i] == x[k] && y[i] == y[j]) ans = max(ans, abs((x[i] - x[j]) * (y[i] - y[k])));
+                if (x[i] == x[k] && y[j] == y[k]) ans = max(ans, abs((x[i] - x[j]) * (y[i] - y[j])));
+                if (x[j] == x[k] && y[i] == y[j]) ans = max(ans, abs((x[i] - x[j]) * (y[i] - y[k])));
+                if (x[j] == x[k] && y[i] == y[k]) ans = max(ans, abs((x[i] - x[j]) * (y[i] - y[j])));
             }
         }
     }
