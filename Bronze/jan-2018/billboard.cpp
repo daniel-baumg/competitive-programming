3ba781063c3863ef56e3432d484e3bca6037783c
Daniel Baumgartner <daniel88baumg@gmail.com>
2021-10-05 23:11:23 -0400

    Update billboard.cpp

--- Bronze/jan-2018/billboard.cpp
+++ Bronze/jan-2018/billboard.cpp
@@ -7,17 +7,15 @@ int main() {
 
     int b1x1, b1y1, b1x2, b1y2, b2x1, b2y1, b2x2, b2y2;
     fin >> b1x1 >> b1y1 >> b1x2 >> b1y2 >> b2x1 >> b2y1 >> b2x2 >> b2y2;
+
     int x = b1x2 - b1x1;
     int y = b1y2 - b1y1;
-    if (b2x1 < b1x1 && b2y1 < b1y1 && b2x2 > b1x1 && b2y2 > b1y2)
-        x -= b2x2 - b1x1;
-    if (b2x2 > b1x2 && b2y2 > b1y2 && b2x1 < b1x2 && b2y1 < b1y1)
-        x -= b1x2 - b2x1;
-    if (b2y1 < b1y1 && b2x1 < b1x1 && b2y2 > b1y1 && b2x2 > b1x2)
-        y -= b2y2 - b1y1;
-    if (b2y2 > b1y2 && b2x2 > b1x2 && b2y1 < b1y2 && b2x1 < b1x1)
-        y -= b1y2 - b2y1;
+    if (b2x1 < b1x1 && b2y1 < b1y1 && b2x2 > b1x1 && b2y2 > b1y2) x -= b2x2 - b1x1;
+    if (b2x2 > b1x2 && b2y2 > b1y2 && b2x1 < b1x2 && b2y1 < b1y1) x -= b1x2 - b2x1;
+    if (b2y1 < b1y1 && b2x1 < b1x1 && b2y2 > b1y1 && b2x2 > b1x2) y -= b2y2 - b1y1;
+    if (b2y2 > b1y2 && b2x2 > b1x2 && b2y1 < b1y2 && b2x1 < b1x1) y -= b1y2 - b2y1;
     x = max(x, 0);
     y = max(y, 0);
+
     fout << x * y << endl;
 }
