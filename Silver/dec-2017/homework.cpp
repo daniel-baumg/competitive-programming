7f4eb687c5aa72a8aae6f4cfccf1353cdb074c99
Daniel Baumgartner <daniel88baumg@gmail.com>
2021-10-26 22:05:40 -0400

    Update homework.cpp

--- Silver/dec-2017/homework.cpp
+++ Silver/dec-2017/homework.cpp
@@ -11,23 +11,23 @@ int main() {
     for (int i = 0; i < n; i++)
         fin >> scores[i];
 
-    vector<double> averages;
+    vector<double> avg;
     double sum = scores[n - 1];
     int min_score = scores[n - 1];
     for (int i = n - 2; i > 0; i--) {
         sum += scores[i];
         min_score = min(min_score, scores[i]);
-        averages.push_back((sum - min_score) / (n - i - 1));
+        avg.push_back((sum - min_score) / (n - i - 1));
     }
-
     double max_avg = 0;
     for (int i = 0; i < n - 2; i++)
-        max_avg = max(max_avg, averages[i]);
+        max_avg = max(max_avg, avg[i]);
+
     vector<int> ans;
     for (int i = 0; i < n - 2; i++)
-        if (averages[i] == max_avg)
+        if (avg[i] == max_avg)
             ans.push_back(n - i - 2);
     sort(ans.begin(), ans.end());
-    for (int i = 0; i < ans.size(); i++)
-        fout << ans[i] << endl;
+    for (int i : ans)
+        fout << i << endl;
 }
