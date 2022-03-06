4fd25d6ee73787befcbf240cd28ddee16ff93dec
Daniel Baumgartner <daniel88baumg@gmail.com>
2022-03-05 21:45:12 -0500

    Update mooyomooyo.cpp

--- Silver/dec-2018/mooyomooyo.cpp
+++ Silver/dec-2018/mooyomooyo.cpp
@@ -22,7 +22,7 @@ int area(bool visited[100][10], int i, int j) {
     int a = 0;
     char c = grid[i][j];
     queue<pair<int, int>> q;
-    q.push(make_pair(i, j));
+    q.push({i, j});
     while (!q.empty()) {
         auto cur = q.front();
         q.pop();
@@ -32,10 +32,10 @@ int area(bool visited[100][10], int i, int j) {
             continue;
         visited[cur.first][cur.second] = true;
         a += 1;
-        q.push(make_pair(cur.first + 1, cur.second));
-        q.push(make_pair(cur.first - 1, cur.second));
-        q.push(make_pair(cur.first, cur.second + 1));
-        q.push(make_pair(cur.first, cur.second - 1));
+        q.push({cur.first + 1, cur.second});
+        q.push({cur.first - 1, cur.second});
+        q.push({cur.first, cur.second + 1});
+        q.push({cur.first, cur.second - 1});
     }
     return a;
 }
@@ -43,17 +43,17 @@ int area(bool visited[100][10], int i, int j) {
 void erase(int i, int j) {
     char c = grid[i][j];
     queue<pair<int, int>> q;
-    q.push(make_pair(i, j));
+    q.push({i, j});
     while (!q.empty()) {
         auto cur = q.front();
         q.pop();
         if (cur.first < 0 || cur.first >= n || cur.second < 0 || cur.second >= 10 || grid[cur.first][cur.second] != c)
             continue;
         grid[cur.first][cur.second] = '0';
-        q.push(make_pair(cur.first + 1, cur.second));
-        q.push(make_pair(cur.first - 1, cur.second));
-        q.push(make_pair(cur.first, cur.second + 1));
-        q.push(make_pair(cur.first, cur.second - 1));
+        q.push({cur.first + 1, cur.second});
+        q.push({cur.first - 1, cur.second});
+        q.push({cur.first, cur.second + 1});
+        q.push({cur.first, cur.second - 1});
     }
 }
 
