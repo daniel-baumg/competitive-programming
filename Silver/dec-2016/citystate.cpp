3383108694bce68eadcf902e74971ee06af02421
Daniel Baumgartner <daniel88baumg@gmail.com>
2022-03-05 21:12:07 -0500

    Update citystate.cpp

--- Silver/dec-2016/citystate.cpp
+++ Silver/dec-2016/citystate.cpp
@@ -11,14 +11,14 @@ int main() {
     for (int i = 0; i < n; i++) {
         string city, state;
         fin >> city >> state;
-        m[make_pair(city.substr(0, 2), state)] += 1;
+        m[{city.substr(0, 2), state}] += 1;
     }
 
     int ans = 0;
     for (auto p : m) {
         if (p.first.first == p.first.second)
             continue;
-        ans += p.second * m[make_pair(p.first.second, p.first.first)];
+        ans += p.second * m[{p.first.second, p.first.first}];
     }
     fout << ans / 2 << endl;
 }
