f31acc3ba2873600fc4a50193935b112b4b826ea
Daniel Baumgartner <daniel88baumg@gmail.com>
2022-06-08 18:52:22 -0400

    Update cowroute1.cpp

--- Bronze/jan-2015/cowroute1.cpp
+++ Bronze/jan-2015/cowroute1.cpp
@@ -7,25 +7,31 @@ int main() {
 
     int a, b, n;
     fin >> a >> b >> n;
+    vector<int> routes[n];
+    int cost[n];
+    for (int i = 0; i < n; i++) {
+        int m;
+        fin >> cost[i] >> m;
+        while (m--) {
+            int k;
+            fin >> k;
+            routes[i].push_back(k);
+        }
+    }
 
     int ans = -1;
-    while (n--) {
-        int cost, m;
-        fin >> cost >> m;
-        int route[m];
-        for (int i = 0; i < m; i++)
-            fin >> route[i];
+    for (int i = 0; i < n; i++) {
         int idxa = -1;
         int idxb = -1;
-        for (int i = 0; i < m; i++) {
-            if (route[i] == a) idxa = i;
-            if (route[i] == b) idxb = i;
+        for (int j = 0; j < routes[i].size(); j++) {
+            if (routes[i][j] == a) idxa = j;
+            if (routes[i][j] == b) idxb = j;
         }
         if (0 <= idxa && idxa < idxb) {
             if (ans < 0)
-                ans = cost;
+                ans = cost[i];
             else
-                ans = min(ans, cost);
+                ans = min(ans, cost[i]);
         }
     }
     fout << ans << endl;
