02c6aaff336bc17e2fc7cf3897cc343d0d67f6f5
Daniel Baumgartner <daniel88baumg@gmail.com>
2022-06-12 22:11:20 -0400

    Update trapped.cpp

--- Bronze/open-2015/trapped.cpp
+++ Bronze/open-2015/trapped.cpp
@@ -7,21 +7,21 @@ int main() {
 
     int n;
     fin >> n;
-    pair<int, int> a[n];
+    pair<int, int> haybales[n];
     for (int i = 0; i < n; i++) {
         int x, y;
         fin >> x >> y;
-        a[i] = {y, x};
+        haybales[i] = {y, x};
     }
-    sort(a, a + n);
+    sort(haybales, haybales + n);
 
     int ans = 0;
     for (int i = 0; i < n - 1; i++) {
         queue<pair<int, int>> q1, q2;
         for (int j = i; j >= 0; j--)
-            q1.push(a[j]);
+            q1.push(haybales[j]);
         for (int j = i + 1; j < n; j++)
-            q2.push(a[j]);
+            q2.push(haybales[j]);
         while (!q1.empty() && !q2.empty()) {
             int d = q2.front().first - q1.front().first;
             if (q1.front().second < d)
@@ -32,7 +32,7 @@ int main() {
                 break;
         }
         if (!q1.empty() && !q2.empty())
-            ans += a[i + 1].first - a[i].first;
+            ans += haybales[i + 1].first - haybales[i].first;
     }
     fout << ans << endl;
 }
